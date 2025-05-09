410e5b92f196c0495cc415a4e848df426a6bfc38
AMattiza <andreas@choose-hub.com>
2025-05-09 14:14:09 +0200

    LicenseChart.jsx aktualisieren

--- src/components/LicenseChart.jsx
+++ src/components/LicenseChart.jsx
@@ -106,7 +106,7 @@ const LicenseChart = ({
 
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={chartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+      <LineChart data={chartData} margin={{ top: 20, right: 30, left: 0, bottom: 5 }}>
         <XAxis dataKey="month" />
         <YAxis tick={false} axisLine={false} tickLine={false} />
         <Tooltip content={<CustomTooltip />} />
